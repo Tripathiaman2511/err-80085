9a39bfe6991eadfe3ab3503fbb8944d7d8cb5140
Slowgamer <aa2232783@gmail.com>
2023-03-18 22:10:45 +0530

    Backend almost complete

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
 contract Account{
@@ -22,21 +22,28 @@ contract Account{
         uint age;
         bool created;
         string description;
-        address[] concernPatient;
+        string[] requestedDataHash;
+      
     }
 
     struct data {
-
+        string dataName;
         string enc_hash;
         string description;
         address owner;
         
     }
 
+    struct requestedData {
+        string dataHash;
+        bool isSeen;
+        address from;
+    }
 
     mapping(address => patient) patientInfo;
     mapping(address => doctor) doctorInfo;
     mapping(string => data) dataInfo;
+    mapping(string => requestedData) requestedDataInfo;
 
     address[] doctorList;
 
@@ -64,11 +71,11 @@ contract Account{
        
     }
 
-    function getDoctorInfo() public view returns (string memory, uint, address[] memory,string memory){
+    function getDoctorInfo() public view returns (string memory, uint,string memory){
         
         address owner = msg.sender;
         
-        return (doctorInfo[owner].name,doctorInfo[owner].age,doctorInfo[owner].concernPatient,doctorInfo[owner].description);
+        return (doctorInfo[owner].name,doctorInfo[owner].age,doctorInfo[owner].description);
     }
 
     function setDoctorInfo(string memory _name,uint _age, bool _created) internal{
@@ -89,11 +96,12 @@ contract Account{
         doctorList.push(owner);
     }
     
-     function add_records(string memory _hash, string memory _description) public {
+     function add_records(string memory _name,string memory _hash, string memory _description) public {
         
         address _owner = msg.sender;
 
         data memory d;
+        d.dataName = _name;
         d.enc_hash = _hash;
         d.description = _description;
         d.owner = _owner;
@@ -124,12 +132,49 @@ contract Account{
         return (_name,_age,_description,_address);
     }
 
-    function getRecords() public view returns (string[] memory,string[] memory){
+    function getRecords() public view returns (string[] memory,string[] memory,string[] memory){
         uint n = patientInfo[msg.sender].dataHash.length;
+        string[] memory _dataName = new string[](n);
+        string[] memory _hash = new string[](n);
         string[] memory _des = new string[](n);
         for(uint i;i<n;i++){
-            _des[i] = dataInfo[patientInfo[msg.sender].dataHash[i]].description;
+            data memory tempData = dataInfo[patientInfo[msg.sender].dataHash[i]];
+            _des[i] = tempData.description;
+            _dataName[i] = tempData.dataName;
+            _hash[i] = tempData.enc_hash;
         }
-        return (patientInfo[msg.sender].dataHash,_des);
+        return (_dataName,_hash,_des);
+    }
+
+    function sendRecord(address _to,string memory _record_hash) public{
+        requestedData memory tempRequested;
+        tempRequested.dataHash = _record_hash;
+        tempRequested.isSeen = false;
+        tempRequested.from = msg.sender;
+        requestedDataInfo[_record_hash] = tempRequested;
+        doctorInfo[_to].requestedDataHash.push(_record_hash);
+    }
+
+    function getDoctorsRequestedData() public view returns (string[] memory,string[] memory, bool[] memory,address[] memory,string[] memory,uint[] memory){
+        uint n = doctorInfo[msg.sender].requestedDataHash.length;
+        string[] memory _dataName = new string[](n);
+        string[] memory _dataHash = new string[](n);
+        bool[] memory _isSeen = new bool[](n);
+        address[] memory _patient= new address[](n);
+        string[] memory _patientName = new string[](n);
+        uint[] memory _patientAge = new uint[](n);
+
+
+        for(uint i; i<n;i++){
+            requestedData memory tempReq = requestedDataInfo[doctorInfo[msg.sender].requestedDataHash[i]];
+            _dataName[i] = dataInfo[tempReq.dataHash].dataName;
+            _dataHash[i] = tempReq.dataHash;
+            _isSeen[i] = tempReq.isSeen;
+            _patient[i] = tempReq.from;
+            _patientName[i] =  patientInfo[tempReq.from].name;
+            _patientAge[i] = patientInfo[tempReq.from].age;
+        }
+
+        return (_dataName,_dataHash,_isSeen,_patient,_patientName,_patientAge);
     }
 }
